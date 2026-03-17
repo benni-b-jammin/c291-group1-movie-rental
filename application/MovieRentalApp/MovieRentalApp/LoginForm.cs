1084d564678ca3964d24edeac97f9230aaaa918a
benni-b-jammin <150716357+benni-b-jammin@users.noreply.github.com>
2026-03-17 16:44:01 -0600

    update nuget packages

--- application/MovieRentalApp/MovieRentalApp/LoginForm.cs
+++ application/MovieRentalApp/MovieRentalApp/LoginForm.cs
@@ -1,10 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
 namespace MovieRentalApp
 {
     public partial class LoginForm : Form
     {
+        public SqlConnection myConnection;
+        public SqlCommand myCommand;
+        public SqlDataReader myReader;
+
         public LoginForm()
         {
             InitializeComponent();
+            /* Starting the connection */
+            SqlConnection myConnection = new SqlConnection("user id=superuser;" + // Username
+                                         "password=dbaccess;" + // Password
+                                         "server=localhost;" + // IP for the server
+                                                               //"Trusted_Connection=yes;" +
+                                         "database=RentalDB; " + // Database to connect to
+                                         "connection timeout=30"); // Timeout in seconds 
+            try
+            {
+                myConnection.Open(); // Open connection
+                myCommand = new SqlCommand();
+                myCommand.Connection = myConnection; // Link the command stream to the connection
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString(), "Error");
+                this.Close();
+            }
         }
     }
 }
