4c13354bdd42dfe2e8c6da067fe2fd23cca1c9f2
Carrollc22 <Carrollc22@mymacewan.ca>
2026-03-22 13:06:57 -0600

    Employee and Customer Test Data
    - Added Employee and Customer test data.
    - couple of same last or first name to test.

--- data/RentalDB_insert.sql
+++ data/RentalDB_insert.sql
@@ -80,7 +80,7 @@ INSERT INTO AppearedIn (MovieID, ActorID)
 -- ============================================================================================================================================
 -- Round 2 - insertions are made using the SELECT procedure as in the provided example insert document - safer solution
 -- ============================================================================================================================================
--- Employee
+-- Employees (4)
 INSERT INTO Employee (EmployeeID, SSN, LastName, FirstName, Address, City, State, ZipCode, StartDate)
 	VALUES (NEXT VALUE FOR EmployeeIDSeq, '789541235', 'Paulson', 'Sam', '10311 106 Avenue', 'Toronto', 'ON', 'M6V9P1', '2026-02-20');
 
@@ -90,7 +90,25 @@ INSERT INTO EmployeePhone (EmployeeID, PhoneNum, PhoneType)
 		'9428521462', 'Home'
 	);
 
--- Customer
+INSERT INTO Employee (EmployeeID, SSN, LastName, FirstName, Address, City, State, ZipCode, StartDate)
+	VALUES (NEXT VALUE FOR EmployeeIDSeq, '787858018', 'Mayo', 'Angela', '5858 21 Avenue', 'Montreal', 'QC', 'J7J5G5', '2026-03-18');
+
+INSERT INTO EmployeePhone (EmployeeID, PhoneNum, PhoneType)
+	VALUES(
+		(SELECT EmployeeID FROM Employee WHERE SSN = '787858018'),
+		'4139250909', 'Home'
+	);
+
+INSERT INTO Employee (EmployeeID, SSN, LastName, FirstName, Address, City, State, ZipCode, StartDate)
+	VALUES (NEXT VALUE FOR EmployeeIDSeq, '135798642', 'Bower', 'Hugh', '757 31 Street', 'Vancouver', 'BC', 'T7H1D4', '2026-01-20');
+
+INSERT INTO EmployeePhone (EmployeeID, PhoneNum, PhoneType)
+	VALUES(
+		(SELECT EmployeeID FROM Employee WHERE SSN = '135798642'),
+		'5207099191', 'Home'
+	);
+
+-- Customers (10)
 INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
 	VALUES (NEXT VALUE FOR CustomerIDSeq, 'McArthur', 'Lena', '123 Lux Lane', 'Vancouver', 'BC', 'V6R8H8', 'mcarthurl@gmail.com', 2, '7814523584152369');
 
@@ -98,6 +116,78 @@ INSERT INTO CustomerPhone (CustomerID, PhoneNum)
 	VALUES(
 		(SELECT CustomerID FROM Customer WHERE Email = 'mcarthurl@gmail.com'),
 		'5207851744'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Roucci', 'Jorges', '954 Long Way', 'Calgary', 'AB', 'P0L8H1', 'RJorges@gmail.com', 3, '4191908976765707');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'RJorges@gmail.com'),
+		'8109598080'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Trundel', 'Tina', '1010 Fish Street', 'Halifax', 'NS', 'H1P7T6', 'TinaT@gmail.com', 4, '1010210250608765');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'TinaT@gmail.com'),
+		'8809196655'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Bowling', 'Will', '7071 66 Street', 'Regina', 'SK', 'Y7A4R5', 'WiBowling@gmail.com', 5, '7565809071355050');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'WiBowling@gmail.com'),
+		'7514098154'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Leterrier', 'Jacques', '21 French Boulevard', 'Montreal', 'QC', 'T8M9L9', 'Jacques@gmail.com', 6, '1362656570708191');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'Jacques@gmail.com'),
+		'4037678080'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Herro', 'Willr', '601 Wide Street', 'Winnipeg', 'MB', 'H9L0P0', 'GHerro@gmail.com', 7, '6564717180809000');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'GHerro@gmail.com'),
+		'6208187007'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Hunt', 'Linda', '400 48 Street', 'Ottawa', 'On', 'M7B6G5', 'Lin@gmail.com', 8, '7897891076765090');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'Lin@gmail.com'),
+		'5900908878'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Lewis', 'Bonnie', '34 New Street', 'Kelowna', 'BC', 'K9L9B8', 'BonBon@gmail.com', 9, '8080808090909090');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'BonBon@gmail.com'),
+		'7609188757'
+	);
+
+INSERT INTO Customer (CustomerID, LastName, FirstName, Address, City, State, ZipCode, Email, AccountNum, CardNum)
+	VALUES (NEXT VALUE FOR CustomerIDSeq, 'Lewis', 'George', '9898 51 Street', 'Edmonton', 'AB', 'T6B9L8', 'GLewis@gmail.com', 10, '5145611208986867');
+
+INSERT INTO CustomerPhone (CustomerID, PhoneNum)
+	VALUES(
+		(SELECT CustomerID FROM Customer WHERE Email = 'GLewis@gmail.com'),
+		'8256156545'
 	);
 
 -- Movie
